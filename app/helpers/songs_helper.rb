879654cec50e35a0815b45c405282f2f1ec52a4a
Matt Farmer <mrfarmer777@gmail.com>
2018-07-17 20:23:02 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,8 @@
 module SongsHelper
-
+  def display_artist(song)
+    if song.artist.present?
+      link_to song.artist.name, artist_path(song.artist)
+    else 
+      link_to edit_song_path(song)
+  end
 end
